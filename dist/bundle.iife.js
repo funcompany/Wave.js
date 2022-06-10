fbbcedbcb664a663d4d53b151f92836903d3ba9f
Travel.Chu <chuchuanming@gmail.com>
2022-06-10 16:45:44 +0800

    radius option

--- dist/bundle.iife.js
+++ dist/bundle.iife.js
@@ -292,13 +292,31 @@ var Wave = (function () {
     let percent = h / 255;
     let width = w / count;
     let skip = true;
+    let radius = options.radius || 0;
+    let radiusObj = { tl: radius, tr: radius, br: radius, bl: radius };
     for (let point = 0; point <= count; point++) {
       let p = data[point]; //get value
       p *= percent;
       let x = width * point;
 
       if (skip) {
-        ctx.rect(x, h / 2 + p / 2, width, -p);
+        let y = h / 2 + p / 2;
+        let height = -p;
+        if (radius > 0) {
+          ctx.beginPath();
+          ctx.moveTo(x + radiusObj.tl, y);
+          ctx.lineTo(x + width - radiusObj.tr, y);
+          ctx.quadraticCurveTo(x + width, y, x + width, y + radiusObj.tr);
+          ctx.lineTo(x + width, y + height - radiusObj.br);
+          ctx.quadraticCurveTo(x + width, y + height, x + width - radiusObj.br, y + height);
+          ctx.lineTo(x + radiusObj.bl, y + height);
+          ctx.quadraticCurveTo(x, y + height, x, y + height - radiusObj.bl);
+          ctx.lineTo(x, y + radiusObj.tl);
+          ctx.quadraticCurveTo(x, y, x + radiusObj.tl, y);
+          ctx.closePath();
+        } else {
+          ctx.rect(x, y, width, height);
+        }
         skip = false;
       } else {
         skip = true;
